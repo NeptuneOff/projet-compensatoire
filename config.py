77c4b6ee17dcd7b893225cfee0f41cbc7872f003
NeptuneOff <kdl33110@gmail.com>
2025-08-31 14:16:48 +0200

    correction clé api

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+BALLEDONTLIE_API_KEY = os.getenv("BALLEDONTLIE_API_KEY")
+
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'ynov.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'change-this-to-a-random-secret'
-BALLEDONTLIE_API_KEY = "9dde42a4-9826-40ea-8b1d-8f27373f7110"
